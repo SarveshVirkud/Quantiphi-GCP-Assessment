2494d2308aeea557ba9657e1bbfc5c6d5caf16f9
SarveshVirkud <45492319+SarveshVirkud@users.noreply.github.com>
2020-01-28 21:11:08 +0530

    Add files via upload

---
+++ GCS & Cloud SQL/bucket_access.py
@@ -0,0 +1,39 @@
+from google.cloud import storage
+import os
+
+BUCKET_NAME = 'sv-bucket'
+DOWNLOAD_FOLDER = 'Folder1'
+DESTINATION_FOLDER = 'Folder1/'
+
+# make destination folder in local machine
+try:
+    os.mkdir(DESTINATION_FOLDER)
+except Exception as e:
+    print('Folder Exists.')
+
+def main():
+    client = storage.Client()
+    bucket = client.get_bucket(BUCKET_NAME)
+    blobs = bucket.list_blobs()
+
+    for b in blobs:
+        
+        fold = DESTINATION_FOLDER + ''
+        
+        if b.name.startswith(DOWNLOAD_FOLDER) and b.name[-1] != '/':
+            
+            folder_struct = b.name.split('/')
+            
+            for folder in folder_struct[:-1]:
+                fold += folder + '/'
+                try:
+                    os.mkdir(fold)
+                except Exception as e:
+                    pass
+            
+            b.download_to_filename(fold + folder_struct[-1])
+
+    print('Replicated')
+
+if __name__ == '__main__':
+    main()
